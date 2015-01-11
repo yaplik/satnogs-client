277ec688d7f92c415446e700db42386620d9b418
John Giannelos <johngiannelos@gmail.com>
2015-01-11 20:08:31 +0200

    Add configuration file for client

---
+++ satnogsclient/settings.py
@@ -0,0 +1,5 @@
+from os import environ
+
+DEMODULATION_COMMAND = environ.get('DEMODULATION_COMMAND', None)
+ENCODING_COMMAND = environ.get('ENCODING_COMMAND', None)
+DECODING_COMMAND = environ.get('DECODING_COMMAND', None)
