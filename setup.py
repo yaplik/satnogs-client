12c08322ccdc8bb73b5a62c0dec9647b35b4a4fb
John Giannelos <johngiannelos@gmail.com>
2015-02-14 13:13:38 +0200

    Add SatNOGS client dependencies.
    * APScheduler
    * SQLAlchemy
    * Requests

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
+
 setup(name='SatNOGS client',
       packages=['satnogsclient'],
       version='0.1',
-      description='SatNOGS Client')
+      description='SatNOGS Client',
+      install_requires=['APScheduler', 'SQLAlchemy', 'requests'])
