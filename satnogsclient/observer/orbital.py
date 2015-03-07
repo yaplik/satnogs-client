9a8b6dd853d30cb853e0eb726f909bf720b14677
icharitopoulos <icharitopoulos@gmail.com>
2015-03-07 12:50:14 +0200

    Implement required orbital tracking functionality.
    * Given observation point data returns alt/az of tracked object.
    * Required data:
    ** observer_dict w/ keys: [lat, lon, elev] of observation point
    ** satellite_dict w/ keys: [tle0, tle1, tle2] of observed object

---
+++ satnogsclient/observer/orbital.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from datetime import datetime
+
+import ephem
+
+
+def pinpoint(observer_dict, satellite_dict, timestamp=None):
+    """
+    Provides azimuth and altitude of tracked object.
+
+    args:
+        observer_dict: dictionary with details of observation point.
+        satellite_dict: dictionary with details of satellite.
+        time: timestamp we want to use for pinpointing the observed object.
+
+        returns:
+            Dictionary containing azimuth, altitude and "ok" for error detection.
+    """
+    # observer object
+    if all(map(lambda x: x in observer_dict, ['lat', 'lon', 'elev'])):
+        observer = ephem.Observer()
+        observer.lon = str(observer_dict['lon'])
+        observer.lat = str(observer_dict['lat'])
+        observer.elevation = float(observer_dict['elev'])
+    else:
+        return {'ok': False}
+
+    # satellite object
+    if all(map(lambda x: x in satellite_dict, ['tle0', 'tle1', 'tle2'])):
+        tle0 = str(satellite_dict['tle0'])
+        tle1 = str(satellite_dict['tle1'])
+        tle2 = str(satellite_dict['tle2'])
+        try:
+            satellite = ephem.readtle(tle0, tle1, tle2)
+        except ValueError:
+            print(('error:', 'ephem object', 'tle values', sys.exc_info()[0]))
+            return {'ok': False}
+        except:
+            print(('error:', 'ephem object', sys.exc_info()[0]))
+            return {'ok': False}
+    else:
+        return {'ok': False}
+
+    # time of observation
+    if not timestamp:
+        timestamp = datetime.now()
+
+    # observation calculation
+    observer.date = timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')
+    satellite.compute(observer)
+    return {'alt': satellite.alt, 'az': satellite.az,
+            'rng': satellite.range, 'rng_vlct': satellite.range_velocity,
+            'ok': True}
