6a7e9b732c8dd03aac5bfa883566ea043a0563a8
icharitopoulos <icharitopoulos@gmail.com>
2015-03-07 16:48:30 +0200

    Implement observer class as an abstract level of rig/rot ctl.
    * Spawns WorkerFreq and WorkerTrack threads.

--- satnogsclient/settings.py
+++ satnogsclient/settings.py
@@ -9,3 +9,8 @@ OUTPUT_PATH = environ.get('SATNOGS_OUTPUT_PATH', '/tmp')
 NETWORK_API_URL = environ.get('SATNOGS_API_URL', 'https://dev.satnogs.org/api/')
 NETWORK_API_QUERY_INTERVAL = 5  # In minutes
 SCHEDULER_SLEEP_TIME = 10  # In seconds
+
+ROT_IP = environ.get('SATNOGS_ROT_IP', '127.0.0.1')
+ROT_PORT = environ.get('SATNOGS_ROT_PORT', 5005)
+RIG_IP = environ.get('SATNOGS_RIG_IP', '127.0.0.1')
+RIG_PORT = environ.get('SATNOGS_RIG_PORT', 6006)
