4cfc1391dfbac93c97421ac9cc148c6e2de9bceb
John Giannelos <johngiannelos@gmail.com>
2016-04-16 18:10:34 +0200

    Bump version 0.2.5 to PyPI.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(name='satnogsclient',
       packages=find_packages(),
-      version='0.2.4',
+      version='0.2.5',
       author='SatNOGS team',
       author_email='client-dev@satnogs.org',
       url='https://github.com/satnogs/satnogs-client/',
