yaplik/satnogs-client

commit c466036be3adb393de2f1d4e6d89e53b37a01fac

Enable using hardware radio.
* Receiver captures line in connected to radio's output, instead of
  using SDR device.

satnogsclient/receiver.py
@@ -77,13 +77,16 @@ class SignalReceiver():
                 '-a': self.pcm_demodulator,
             }
 
-            args = ['{0}{1}'.format(key, params[key]) for key in params]
+            args = []
+            for key in params:
+                args.append(key)
+                args.append(params[key])
 
             if self.aprs:
                 args.append('-A')
 
             args.append('-')
-            ret = [settings.ENCODING_COMMAND] + args
+            cmd = [settings.ENCODING_COMMAND] + args
         else:
             # oggenc --raw-endianess=0 -R 24k -B 16 -C 1 -r -q 4
             params = {
@@ -93,21 +96,35 @@ class SignalReceiver():
                 '-q': '4'
             }
             args = ['--raw-endianness=0', '-r']
-            args += ['{0} {1}'.format(key, params[key]) for key in params]
-            args += ['-']
-            ret = [settings.ENCODING_COMMAND] + args
-        return ret
+            for key in params:
+                args.append(key)
+                args.append(params[key])
+
+            args.append('-')
+            cmd = [settings.ENCODING_COMMAND] + args
+        return cmd
 
     def get_demodulation_cmd(self):
         """Provides demodulation command."""
-        params = {
-            '-f': self.frequency,
-            '-p': self.ppm_error,
-            '-s': self.sample_rate,
-            '-M': self.modulation,
-            '-g': '40'
-        }
-        args = ['{0} {1}'.format(key, params[key]) for key in params]
+        if settings.HARDWARE_RADIO:
+            params = {
+                '-f': 'cd',
+                '-t': 'raw'
+            }
+        else:
+            params = {
+                '-f': self.frequency,
+                '-p': self.ppm_error,
+                '-s': self.sample_rate,
+                '-M': self.modulation,
+                '-g': '40'
+            }
+
+        args = []
+        for key in params:
+            args.append(key)
+            args.append(params[key])
+
         return [settings.DEMODULATION_COMMAND] + args
 
     def get_output_path(self, receiving=True):

satnogsclient/settings.py
@@ -30,6 +30,8 @@ for p in [APP_PATH, OUTPUT_PATH, COMPLETE_OUTPUT_PATH, INCOMPLETE_OUTPUT_PATH]:
 VERIFY_SSL = bool(strtobool(environ.get('SATNOGS_VERIFY_SSL', 'True')))
 DEFAULT_SQLITE_PATH = path.join(APP_PATH, 'jobs.sqlite')
 SQLITE_URL = environ.get('SATNOGS_SQLITE_URL', 'sqlite:///' + DEFAULT_SQLITE_PATH)
+
+HARDWARE_RADIO = bool(strtobool(environ.get('SATNOGS_HARDWARE_RADIO', False)))
 DEMODULATION_COMMAND = environ.get('SATNOGS_DEMODULATION_COMMAND', 'rtl_fm')
 ENCODING_COMMAND = environ.get('SATNOGS_ENCODING_COMMAND', 'oggenc')
 DECODING_COMMAND = environ.get('SATNOGS_DECODING_COMMAND', 'multimon-ng')

commit d38473284a82a878262f94c61df73e6f41ce29b3

Fix argument formatting in receiver.py
Seems like a regression in c466036, the way we format the arguments
to the demodulation_cmd and decoding_cmd changed. With this, my
default configuration bailed on attempting arguments not as strings
(the gain maybe?). In this commit I restore the argument formatting
we had previously, which does work with the same config.

satnogsclient/receiver.py
@@ -77,10 +77,7 @@ class SignalReceiver():
                 '-a': self.pcm_demodulator,
             }
 
-            args = []
-            for key in params:
-                args.append(key)
-                args.append(params[key])
+            args = ['{0} {1}'.format(key, params[key]) for key in params]
 
             if self.aprs:
                 args.append('-A')
@@ -96,11 +93,8 @@ class SignalReceiver():
                 '-q': '4'
             }
             args = ['--raw-endianness=0', '-r']
-            for key in params:
-                args.append(key)
-                args.append(params[key])
-
-            args.append('-')
+            args += ['{0} {1}'.format(key, params[key]) for key in params]
+            args += ['-']
             cmd = [settings.ENCODING_COMMAND] + args
         return cmd
 
@@ -124,10 +118,7 @@ class SignalReceiver():
                 '-g': '40'
             }
 
-        args = []
-        for key in params:
-            args.append(key)
-            args.append(params[key])
+        args = ['{0} {1}'.format(key, params[key]) for key in params]
 
         return [settings.DEMODULATION_COMMAND] + args
 