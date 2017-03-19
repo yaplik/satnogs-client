40afc357e0850c71153f8779583fc03f643b2271
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2017-03-19 15:11:04 +0200

    Reorder and group metadata and options together

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 from setuptools import find_packages, setup
 
 
-setup(name='satnogsclient',
-      packages=find_packages(),
-      version='0.2.5',
-      author='SatNOGS team',
-      author_email='client-dev@satnogs.org',
-      url='https://github.com/satnogs/satnogs-client/',
-      description='SatNOGS Client',
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'APScheduler',
-          'SQLAlchemy',
-          'requests',
-          'validators',
-          'python-dateutil',
-          'ephem',
-          'pytz',
-          'flask',
-          'pyopenssl',
-          'pyserial',
-          'flask-socketio',
-          'redis'
-      ],
-      extras_require={
-          'develop': 'flake8'
-      },
-      scripts=['satnogsclient/bin/satnogs-poller'])
+setup(
+    name='satnogsclient',
+    version='0.2.5',
+    url='https://github.com/satnogs/satnogs-client/',
+    author='SatNOGS team',
+    author_email='client-dev@satnogs.org',
+    description='SatNOGS Client',
+    zip_safe=False,
+    install_requires=[
+        'APScheduler',
+        'SQLAlchemy',
+        'requests',
+        'validators',
+        'python-dateutil',
+        'ephem',
+        'pytz',
+        'flask',
+        'pyopenssl',
+        'pyserial',
+        'flask-socketio',
+        'redis'
+    ],
+    extras_require={
+        'develop': 'flake8'
+    },
+    scripts=['satnogsclient/bin/satnogs-poller'],
+    include_package_data=True,
+    packages=find_packages()
+)
