bb8ae9fd5ae9759f6362a3920de72ce85a156900
Nikos Roussos <nikos@roussos.cc>
2017-10-02 17:33:11 +0300

    Remove user documentation
    * Add some initial docs for development environment

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='satnogsclient',
-    version='0.3',
+    version='0.5',
     url='https://gitlab.com/librespacefoundation/satnogs/satnogs-client',
     author='SatNOGS project',
     author_email='dev@satnogs.org',
