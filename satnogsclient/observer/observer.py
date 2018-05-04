d13563bc332b4eaa192b28c731ed15e13df45910
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-05-05 00:54:03 +0300

    Fix typo in client metadata
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- satnogsclient/observer/observer.py
+++ satnogsclient/observer/observer.py
@@ -154,7 +154,7 @@ class Observer(object):
             url += '/'
         client_metadata = gnuradio_handler.get_gnuradio_info()
         client_metadata['latitude'] = settings.SATNOGS_STATION_LAT
-        client_metadata['longtitude'] = settings.SATNOGS_STATION_LON
+        client_metadata['longitude'] = settings.SATNOGS_STATION_LON
         client_metadata['elevation'] = settings.SATNOGS_STATION_ELEV
         resp = requests.put(
             url, headers=headers,
