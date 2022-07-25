a70c9481b310c91822d4c1c12e01f322290c0188
user <user@pekasat.com>
2022-07-25 07:40:19 +0200

    Misfire grace

--- satnogsclient/scheduler/__init__.py
+++ satnogsclient/scheduler/__init__.py
@@ -15,7 +15,7 @@ EXECUTORS = {
     'default': ThreadPoolExecutor(20),
 }
 
-JOB_DEFAULTS = {'coalesce': True, 'max_instances': 1, 'misfire_grace_time': 30}
+JOB_DEFAULTS = {'coalesce': True, 'max_instances': 1, 'misfire_grace_time': 180}
 
 SCHEDULER = BackgroundScheduler(jobstores=JOBSTORES,
                                 executors=EXECUTORS,
